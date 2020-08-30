7fb1f1405e55ce5a6a4a8abce18f62338fa6dc17
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-30 00:45:25 -0500

    Adding more features in route

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 'use strict'
+const Joi = require('@hapi/joi')
 const site = require('./controllers/site')
 const user = require('./controllers/user')
+
 
 module.exports  = [
   {
@@ -16,6 +18,20 @@ module.exports  = [
   },
   {
     method: 'POST',
+    options: {
+      validate: {
+        payload: Joi.object({
+          name: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required(),
+          email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+          password: Joi.string().required().min(6)
+        })
+      }
+    },
     path: '/create-user',
     handler: user.createUser
   },
